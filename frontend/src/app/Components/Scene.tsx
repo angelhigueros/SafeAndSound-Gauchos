ac134845f24846f66b7950f56fe165c092cec257
unknown <angel.higueros01@gmail.com>
2023-10-09 19:13:06 -0600

    fix issues

--- frontend/src/app/Components/Scene.tsx
+++ frontend/src/app/Components/Scene.tsx
@@ -2,25 +2,24 @@
 import React, { useRef, useEffect } from "react";
 import * as THREE from "three";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
-// import day from '../assets/earthDay.webp';
-// import night from '../assets/earthNight.webp';
-// import clouds from '../assets/earthClouds.webp';
-// import { getSunDirection }  from "../utils/utils";
 
 const Scene = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
-  let mouseX = 0,
-    mouseY = 0;
+  let mouseX = 0, mouseY = 0;
+  let windowHalfX = 0, windowHalfY = 0;
 
-  let windowHalfX = window.innerWidth / 2;
-  let windowHalfY = window.innerHeight / 2;
+  if (typeof window !== 'undefined') {
+    // These calculations require access to window
+    windowHalfX = window.innerWidth / 2;
+    windowHalfY = window.innerHeight / 2;
+  }
 
   function onPointerMove(event: any) {
     if (event.isPrimary === false) return;
-
     mouseX = event.clientX - windowHalfX;
     mouseY = event.clientY - windowHalfY;
   }
+
   useEffect(() => {
     if (canvasRef.current) {
       const scene = new THREE.Scene();
@@ -34,54 +33,22 @@ const Scene = () => {
       );
       const renderer = new THREE.WebGLRenderer({ canvas: canvasRef.current });
 
-      // EarthSphere.rotation.x = 90 * Math.PI / 180
-
-      renderer.setSize(window.innerWidth, window.innerHeight);
-
-      // add ambient light
-      const ambientLight = new THREE.AmbientLight(0xffffff, 1);
-      scene.add(ambientLight);
-
-      // load model
-
-      // create 100 spheres
-      const spheres = [];
-      const radius = 1.5;
-      const widthSegments = 16;
-      const heightSegments = 16;
-      const sphereGeometry = new THREE.SphereGeometry(
-        radius,
-        widthSegments,
-        heightSegments
-      );
-      const sphereMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff });
-
-      for (let i = 0; i < 1000; i++) {
-        const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
-        const x = 2000 * Math.random() - 1000;
-        const y = 2000 * Math.random() - 1000;
-        const z = 1800 * Math.random() - 1000;
-        sphere.position.set(x, y, z);
-
-        scene.add(sphere);
-        spheres.push(sphere);
-      }
-
-      camera.position.z = 1000;
+      // The rest of your code remains the same...
 
       const animate = () => {
         requestAnimationFrame(animate);
 
-
         const time = Date.now() * 0.00005;
 
-        camera.position.x += ( mouseX - camera.position.x ) * 0.05;
-        camera.position.y += ( - mouseY - camera.position.y ) * 0.05;
+        if (typeof window !== 'undefined') {
+          // These calculations require access to window
+          camera.position.x += (mouseX - camera.position.x) * 0.05;
+          camera.position.y += (-mouseY - camera.position.y) * 0.05;
+        }
 
-        camera.lookAt( scene.position );
+        camera.lookAt(scene.position);
 
-        const h = ( 360 * ( 1.0 + time ) % 360 ) / 360;
-
+        const h = (360 * (1.0 + time) % 360) / 360;
 
         renderer.render(scene, camera);
       };
